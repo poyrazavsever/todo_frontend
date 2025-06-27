f5650f83b3cfc2466638a2710bccf14280da4cf6
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-27 10:04:30 +0300

    feat: added todo card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
+import TodoCard from '@/components/TodoCard';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
       <div className="py-8">
-        <h1 className="text-3xl font-bold mb-4">Welcome to My Next.js App</h1>
-        <p className="text-gray-600">This is a simple example of a Next.js application.</p>
+        <TodoCard />
       </div>
     </Layout>
   );
